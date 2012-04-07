7f28a9c44707d2ce2ef03b25b02ee967b301902b
Geoff Huston <Geoff Huston geoff.huston@gmail.com>
2012-04-07 17:57:29 -0400

    Starting to use the irc package. Getting ready to make the irc handler less horrible

--- pancake.hs
+++ pancake.hs
@@ -1,14 +1,15 @@
 module Main (main) where
 
+import Control.Monad.Fix (fix)
+
+import Data.List
+
 import Network
+import qualified Network.IRC as Irc
 
 import System.Environment (getArgs)
 import System.IO
 import System.Random
-
-import Control.Monad.Fix (fix)
-
-import Data.List
 
 import Text.Printf
 
@@ -30,28 +31,40 @@ nick   = "pancake"
 main :: IO ()
 main = do
    args <- getArgs
-   let server = args !! 0
-       port   = read (args !! 1)
+   let server       = args !! 0
+       port         = read (args !! 1)
    h <- connectTo server (PortNumber (fromIntegral port))
    training <- readFile "learned.map"
    rng <- getStdGen
    hSetBuffering h NoBuffering
-   write h "NICK" nick
-   write h "USER" (nick++" 0 * :pancake")
-   write h "JOIN" chan
+   
+   initCommands h "pancake"
+   
    listen rng h $ read training
 
+initCommands :: Handle -> String -> IO ()
+initCommands h nick = do
+   let nickCommand = Irc.nick nick
+       userCommand = Irc.user nick "0" "*" nick
+       joinCommand = Irc.joinChan "#botTest"
+   mapM_ (write_ h . Irc.encode) [nickCommand, userCommand, joinCommand]
+
 -- | Purely a utility function
-mkTrainingMap :: String -> IO ()
-mkTrainingMap path = do
-   training <- readFile "training.txt"
+mkTrainingMap :: String -> String -> IO ()
+mkTrainingMap inPath outPath = do
+   training <- readFile inPath
    let ls  = lines training
    let seq = concatMap mkSequenceLine ls
    let tr  = Markov.train seq
-   writeFile path $ show tr
+   writeFile outPath $ show tr
 
 mkSequenceLine :: String -> [Sequence String]
 mkSequenceLine line = Start : (map Block $ words line) ++ [End]
+
+write_ :: Handle -> String -> IO ()
+write_ h s = do
+        hPrintf h "%s \r\n" s
+        printf    "> %s \n" s
 
 write :: Handle -> String -> String -> IO ()
 write h s t = do
@@ -62,20 +75,21 @@ listen :: StdGen -> Handle -> PancakeMap -> IO ()
 listen g h m = forever (g, m) $ \(rng, x) -> do
         t <- hGetLine h
         let s = init t
-        (newRng, newM) <- eval rng h m (clean s)
+        (newRng, newM) <- (if ping s then (pong s >> return (g, m)) else eval rng h m (clean s))
         writeFile "learned.map" $ show newM
         putStrLn s
         return (newRng, newM)
     where
         forever x a = a x >>= (flip forever) a
         clean = drop 1 . dropWhile (/= ':') . drop 1
+        ping x = "PING :" `isPrefixOf` x
+        pong x = write h "PONG" (':' : drop 6 x)
 
 eval :: StdGen -> Handle -> PancakeMap -> String -> IO (StdGen, PancakeMap)
 eval g h m x | "pancake" `isInfixOf` x = do
         (gen, response) <- generate g m
         privmsg h response
         return (gen, m)
-eval g h m x | "PING :" `isPrefixOf` x = write h "PONG" (':' : drop 6 x) >> return (g, m)
 eval g _ m x = return (g, Markov.trainWithMap m $ mkSequenceLine x)
 
 privmsg :: Handle -> String -> IO ()
